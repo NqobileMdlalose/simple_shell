483660694a7681b26ed55f836259f4f73ffd3bf8
NqobileMdlalose <mdlalose150@gnail.c>
2023-07-16 06:35:33 -0700

    shell

--- qbshell.h
+++ qbshell.h
@@ -1,15 +1,5 @@
-#ifndef _QBSHELL_H_
+#ifndef _QBSHELL_H
 #define _QBSHELL_H
 extern char **environ;
-/*Header files*/
-
-
-/* Function Declarations*/
-
-
-/*Built-ins*/
-
-
-
-#endif /*qbshell.h*/
-
+void execute_command(char *args[], char *err_mesg);
+#endif
