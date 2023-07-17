49617064a0cb1b2934a3f9c1f4b2245ec22ffbdf
NqobileMdlalose <mdlalose150@gnail.c>
2023-07-17 02:47:52 -0700

    fixes

--- qb_parse_command.c
+++ qb_parse_command.c
@@ -13,33 +13,39 @@
 
 void parse_cmd(const char *line, char *args[], int *argc)
 {
-    char *token, *command_copy = NULL;
-    const char *delimiter = " \n";
-    size_t line_length = strlen(line); /*change to qb_strlen()*/
-    command_copy = (char *)malloc(line_length + 1);
+	char *token, *command_copy = NULL;
+	const char *delimiter = " \n";
+	size_t line_length = strlen(line); /*change to qb_strlen()*/
+	int i;
 
-    if (command_copy == NULL)
-    {
-        perror("Parse Command Input: Memory allocation error");
-        return;
-    }
-    strcpy(command_copy, line); /* copy input line to command_copy*/
-    *argc = 0;
-    token = strtok(command_copy, delimiter);
+	command_copy = (char *)malloc(line_length + 1);
+	if (command_copy == NULL)
+	{
+		perror("Parse Command Input: Memory allocation error");
+		return;
+	}
+	strcpy(command_copy, line); /* copy input line to command_copy*/
+	*argc = 0;
+	token = strtok(command_copy, delimiter);
 
-    while (token != NULL && *argc < (MAX_ARGUMENTS - 1))
-    {
-    args[*argc] = (char *)malloc(strlen(token) + 1); /* change to qb_strlen()*/
-    if (args[*argc] == NULL) 
-    {
-        perror("Parse Command Input: Memory allocation error");
-        return;
-    }
-    strcpy(args[*argc], token); /*change to qb_strcpy()*/
-    (*argc)++;
-    token = strtok(NULL, delimiter);
-    }
-    args[*argc] = NULL;
-    free(command_copy);
-    free(args);
+	while (token != NULL && *argc < (MAX_ARGUMENTS - 1))
+	{
+		args[*argc] = (char *)malloc(strlen(token) + 1); /* change to qb_strlen()*/
+		if (args[*argc] == NULL) 
+		{
+			perror("Parse Command Input: Memory allocation error");
+			for (i = 0; i < *argc;i++)
+			{
+				free(args[i]);
+			}
+			free(args);
+			free(command_copy);
+			return;
+		}
+		strcpy(args[*argc], token); /*change to qb_strcpy()*/
+		(*argc)++;
+		token = strtok(NULL, delimiter);
+	}
+	args[*argc] = NULL;
+	free(command_copy);
 }
