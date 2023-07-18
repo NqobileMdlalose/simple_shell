6ec5fd1e11e17ff14975c07203749cd46279cb7c
pbliphadzi <pbliphadzi@gmail.com>
2023-07-18 01:31:24 -0700

    Update to header file : adding prototypes

--- qbshell.h
+++ qbshell.h
@@ -6,4 +6,6 @@ void qb_exit(char **args);
 void parse_cmd(const char *line, char *args[], int *argc);
 int qb_strcmp(const char *str1, const char *str2);
 void execute_command(char *args[], char *err_mesg);
+char *qb_strcpy(char *dest, const char *src);
+int qb_strlen(const char *string);
 #endif
