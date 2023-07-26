3948749243796e0dc44f7b79aec912f110c8f619
NqobileMdlalose <mdlalose150@gnail.c>
2023-07-26 13:56:39 -0700

    fixing non interactive

--- shell.c
+++ shell.c
@@ -1,5 +1,4 @@
 #include "qbshell.h"
-#include <stdbool.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -20,16 +19,16 @@ void handle_non_interactive(char *argv[], int argc)
 	char *args[MAX_COMMAND_LENGTH];
 
 	errno = ENOENT;
-	while ((read_len = getline(&line, &len, stdin)) != 1 && read_len != EOF)
+	while ((read_len = getline(&line, &len, stdin)) != 1)
 	{
 		if (line[read_len - 1] == '\n')
 			line[read_len - 1] = '\0';
 		parse_cmd(line, args, &argc);
 		if (qb_strcmp(args[0], "env") == 0)
 			print_environment();
-		if (qb_strcmp(args[0], "exit") == 0)
+		if (qb_strcmp(args[0], "exit") == 0 && argc == 2)
 			qb_exit(args);
-		if (argc >= 1)
+		if (argc >= 1 && qb_strcmp(args[0], "env") != 0)
 			execute_command(args, err_mesg);
 	}
 	free(line);
